831e4577a061df196906fc503581ba856ab0d70d
Milton Inostroza <milinostroza@groupon.com>
2015-09-27 22:00:49 -0500

    does pass original binary

--- src/eb.erl
+++ src/eb.erl
@@ -1,5 +1,4 @@
 -module('eb').
--compile(export_all).
 
 %% API exports
 -export([exif_data_for/1]).
@@ -17,7 +16,6 @@
 %%====================================================================
 %% API functions
 %%====================================================================
-
 exif_data_for(FilePath) ->
   {ok, Fd} = file:open(FilePath, [read, binary, raw]),
   {ok, FileContent} = file:read(Fd, 10000),
@@ -46,14 +44,14 @@ extract_tiff_header(<<ByteAlign:2/binary, TagMark:2/binary, OffsetToIFD:4/binary
   {TiffHeader, Rest}.
 
 extract_image_file_directory(<<DirectoryEntriesNumber:16/integer, Rest/binary>>, Original) ->
-  foo(DirectoryEntriesNumber, Rest, Original, []).
+  extract_image_file_directory(DirectoryEntriesNumber, Rest, Original, []).
 
-foo(1, Rest, Original, Acc) ->
+extract_image_file_directory(1, Rest, _Original, Acc) ->
   {Acc, Rest};
-foo(DirectoryEntries, Rest, Original, Acc) ->
+extract_image_file_directory(DirectoryEntries, Rest, Original, Acc) ->
   {Directory, NewRest} = extract_directory_entry(Rest),
-  NewDirectory = process_directory_entry(Directory, NewRest, Original),
-  foo(DirectoryEntries - 1, NewRest, Original, [NewDirectory | Acc]).
+  NewDirectory = process_directory_entry(Directory, Original),
+  extract_image_file_directory(DirectoryEntries - 1, NewRest, Original, [NewDirectory | Acc]).
 
 extract_directory_entry(?DIRECTORY_ENTRY_PATTERN) ->
   {DataTypeName, BytesPerComponent} = data_type(DataFormat),
@@ -66,31 +64,24 @@ extract_directory_entry(?DIRECTORY_ENTRY_PATTERN) ->
  },
  {DirectoryEntry, Rest}.
 
-process_directory_entry(#{data_type := ascii_strings, data_length := Len, data := DirectoryData} = DirectoryEntry, Rest, Original) ->
+process_directory_entry(#{data_type := ascii_strings, data_length := Len, data := DirectoryData} = DirectoryEntry, Bin) ->
   NewData = case Len > bits(4) of
     true ->
       Offset = binary:decode_unsigned(DirectoryData, big) - 8,
       NewLen = Len - 1,
-      <<_:Offset/binary, Data:NewLen/binary, _/binary>> = Original,
+      <<_:Offset/binary, Data:NewLen/binary, _/binary>> = Bin,
       Data;
     false ->
       <<Data:Len/binary, _/binary>> = DirectoryData,
       Data
   end,
   DirectoryEntry#{data => NewData};
-process_directory_entry(#{data_type := unsigned_short, data_length := Len, data := DirectoryData} = DirectoryEntry, Rest, Original) ->
-  NewData = case Len > bits(4) of
-    true ->
-      Offset = binary:decode_unsigned(DirectoryData, big) - 22,
-      NewLen = Len - 1,
-      <<_:Offset/binary, Data:NewLen/binary, _/binary>> = Original,
-      Data;
-    false ->
-      <<Data:Len/binary, _/binary>> = DirectoryData,
-      Data
-  end,
-  DirectoryEntry#{data => binary:decode_unsigned(NewData)};
-process_directory_entry(_, _, _) -> undefined.
+process_directory_entry(#{data_type := unsigned_short, data_length := Len, data := DirectoryData} = DirectoryEntry, _Bin) ->
+  <<Data:Len/binary, _/binary>> = DirectoryData,
+  DirectoryEntry#{data => binary:decode_unsigned(Data)};
+process_directory_entry(#{data_type := unsigned_rational, data_length := _Len, data := DirectoryData} = DirectoryEntry, _Bin) ->
+  DirectoryEntry#{data => binary:decode_unsigned(DirectoryData)};
+process_directory_entry(_, _) -> undefined.
 
 
 tag_name(16#010F) -> make;
@@ -101,13 +92,13 @@ tag_name(16#011B) -> y_resolution;
 tag_name(16#0128) -> resolution_unit;
 tag_name(16#0131) -> software;
 tag_name(16#0132) -> date_time;
-tag_name(16#0213) -> y_cb_cr_positioning;
+tag_name(16#0213) -> ycb_cr_positioning;
 tag_name(_) -> undefined.
 
 data_type(2) -> {ascii_strings, 1};
 data_type(3) -> {unsigned_short, 2};
 data_type(5) -> {unsigned_rational, 8};
-data_type(_) -> undefined.
+data_type(X) -> erlang:display(X), undefined.
 
 bits(Number) -> Number.
 
